6c62fb99f6cae0d78e114b7d1e1a53084970efc6
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-02-24 20:22:01 -0300

    exercicio 1

--- exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
@@ -40,8 +40,48 @@ let soma = alfa + beta + omega;
 console.log(soma);
 
 if (soma == 180){
-  Boolean
+  console.log(Boolean(positive));
   
 } else {
-  console.log(Boolean)
+  console.log(Boolean(false));
 }
+
+let peca = "rei";
+
+if (peca == "piao") {
+  console.log("Primeiro movimento: até duas casas reto.Demais movimentos: uma casa reto.")
+} else if (peca == "torre") {
+  console.log("Reto até o fim, na vertical e horizontal.");
+} else if (peca == "cavalo") {
+  console.log("Quatro casas em L.");
+} else if (peca == "bispo") {
+  console.log("Nas diagonais até o fim.");
+} else if (peca == "rainha") {
+  console.log("Movimento livre até o fim.");
+} else if (peca == "rei") {
+  console.log("Uma casa livre.");
+} else {
+  console.log("Entrada invalida.");
+}
+
+switch (peca) {
+  case "piao":
+    console.log("Primeiro movimento: até duas casas reto.Demais movimentos: uma casa reto.");
+    break;
+  case "torre":
+    console.log("Reto até o fim, na vertical e horizontal.");
+    break;
+  case "cavalo":
+    console.log("Quatro casas em L.");
+    break;
+  case "bispo":
+    console.log("Nas diagonais até o fim.");
+    break;
+  case "rainha":
+    console.log("Movimento livre até o fim.");
+    break;
+  case "rei":
+    console.log("Uma casa livre.");
+    break;
+}
+
