ab000876ae87716e5bf8e92f99177e6f4e29c413
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-02-24 16:06:09 -0300

    arquivo .js

---
+++ exercises/bloco_4/4.1/scripts.js
@@ -0,0 +1,10 @@
+let age = 36;
+let name = "Jorge";
+let birthday = 20;
+let birthyear = 85;
+
+console.log(age);
+console.log(name);
+console.log(birthday);
+console.log(birthyear);
+console.log("O "+name+" tem "+age+" anos e nasceu em"+birthday+" de "+birthyear);
