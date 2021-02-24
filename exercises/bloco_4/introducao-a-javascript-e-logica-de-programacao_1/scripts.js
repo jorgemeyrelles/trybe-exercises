539d6b8c6951930e02fe39e7e6ed19c8ebaf4d9f
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-02-24 19:28:30 -0300

    exercicio 1

--- exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
@@ -1,5 +1,9 @@
 let a = 20;
 let b = 3;
+let c = 10;
+let alfa = 30;
+let beta = 60;
+let omega = 80;
 
 //adição
 console.log(a+b);
@@ -15,3 +19,29 @@ console.log(a/b);
 
 //módulo
 console.log(a%b);
+
+if(a > b && a > c) {
+  console.log("a = "+a);
+} else if (b > a && b > c) {
+  console.log("b = "+b);
+} else if (c > a && c > b) {
+  console.log("c = "+c);
+}
+
+if (a > 0) {
+  console.log("Positivo");
+} else if (a < 0) {
+  console.log("Negativo");
+} else {
+  console.log("0");
+}
+
+let soma = alfa + beta + omega;
+console.log(soma);
+
+if (soma == 180){
+  Boolean
+  
+} else {
+  console.log(Boolean)
+}
