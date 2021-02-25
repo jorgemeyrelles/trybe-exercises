e0d08bb40c21296221e463844ed38aa669be4f85
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-02-25 00:15:23 -0300

    exercicio 1

--- exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
@@ -85,3 +85,44 @@ switch (peca) {
     break;
 }
 
+var nota = 85;
+if (nota >= 0 && nota < 50) {
+  console.log("F");
+} else if (nota >= 50 && nota < 60) {
+  console.log("E");
+} else if (nota >= 60 && nota < 70) {
+  console.log("D");
+} else if (nota >= 70 && nota < 80) {
+  console.log("C");
+} else if (nota >= 80 && nota < 90) {
+  console.log("B");
+} else if (nota >= 90 && nota <= 100) {
+  console.log("A");
+} else {
+  console.log("ENTRADA INVÁLIDA! A NOTA DEVE ESTAR ENTRE 0 E 100.");
+}
+
+var j = 3;
+var k = 7;
+var l = 1;
+
+if (j%2 == 0 || k%2 == 0 || l%2 == 0) {
+  console.log(Boolean(true));
+} else {
+  console.log(Boolean(false));
+}
+
+if (j%2 != 0 || k%2 != 0 || l%2 != 0) {
+  console.log(Boolean(true));
+} else {
+  console.log(Boolean(false));
+}
+
+var valorCusto = 10;
+var valorVenda = 25;
+var impostoSobreOCusto = valorCusto * 0.2;
+var valorCustoTotal = valorCusto * 1.2;
+var lucro = valorVenda - valorCustoTotal;
+console.log("custo total = "+valorCustoTotal);
+console.log("imposto sobre o custo = "+impostoSobreOCusto);
+console.log(lucro);
