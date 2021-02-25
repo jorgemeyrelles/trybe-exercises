c0a56a96370a4484ad1b12eac12424c42dbb5ef3
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-02-25 14:39:49 -0300

    exercicio 1-fim

--- exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_1/scripts.js
@@ -127,3 +127,35 @@ console.log("custo total = "+valorCustoTotal);
 console.log("imposto sobre o custo = "+impostoSobreOCusto);
 console.log(lucro);
 
+var salarioBruto = 3000.00;
+var salarioBase = 0;
+var salarioLiquido = 0;
+
+if (salarioBruto <= 1556.94) {
+  salarioBase = salarioBruto * (1-0.08);
+} else if (salarioBruto > 1556.94 && salarioBruto <= 2594.92) {
+  salarioBase = salarioBruto * (1-0.09);
+} else if (salarioBruto > 2594.92 && salarioBruto <= 5189.82) {
+  salarioBase = salarioBruto * (1-0.11);
+} else if (salarioBruto > 5189.82) {
+  salarioBase = salarioBruto - 570.88;
+} else {
+  console.log("Entrada invalida!");
+}
+console.log(salarioBase);
+
+if (salarioBase <= 1903.98) {
+  salarioLiquido = salarioBase;
+} else if (salarioBase > 1903.98 && salarioBase <= 2826.65) {
+  salarioLiquido = salarioBase - ((salarioBase * 0.075) - 142.80);
+} else if (salarioBase > 2826.65 && salarioBase <= 3751.05) {
+  salarioLiquido = salarioBase - ((salarioBase * 0.15) - 354.80);
+} else if (salarioBase > 3751.05 && salarioBase <= 4664.68) {
+  salarioLiquido = salarioBase - ((salarioBase * 0.225) - 636.13);
+} else if (salarioBase > 4664.68) {
+  salarioLiquido = salarioBase - ((salarioBase * 0.275) - 869.36);
+} else {
+  console.log("Entrada invalida!");
+}
+
+console.log(salarioLiquido);
