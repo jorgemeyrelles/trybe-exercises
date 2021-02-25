af3340fb9edc11d974f7558ac304de6c814594ec
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-02-25 15:12:16 -0300

    exercicios

---
+++ exercises/bloco_4/introducao-a-javascript-e-logica-de-programacao_2/scripts.js
@@ -0,0 +1,19 @@
+let menu = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let menuServices = menu.length;
+
+console.log(menuServices);
+
+let menu2 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+let indexOfPortfolio = menu2.indexOf('Portfólio');
+
+console.log(indexOfPortfolio);
+
+let menu3 = ['Home', 'Serviços', 'Portfólio', 'Links'];
+menu3.push('Contato');
+
+console.log(menu3);
+
+let groceryList = ["Arroz", "Feijão", "Alface", "Melancia"];
+for(var i = 0; i < groceryList.length ; i++) {
+  console.log(groceryList[i]);
+}
