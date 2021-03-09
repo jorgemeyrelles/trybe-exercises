a37d9fbd86a794523526fa378fec7e9b3a35ca6b
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-03-09 14:23:11 -0300

    exercicio 1 e 2

--- exercises/5.3/javascript-dom-eventos-e-web-storage_3/exercise_1.js
+++ exercises/5.3/javascript-dom-eventos-e-web-storage_3/exercise_1.js
@@ -11,6 +11,51 @@ function createDaysOfTheWeek() {
   };
 };
 
-createDaysOfTheWeek();
+function createDaysOfMonth() {
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const daysWeek = document.querySelector('#days');
+  // Escreva seu código abaixo.
+  console.log(dezDaysList);
+  for (let i = 0; i < dezDaysList.length; i += 1) {
+    const txt = dezDaysList[i];
+    
+    const tit = document.createElement('li');
+    if (txt === 24 | txt === 25 | txt === 31) {
+      tit.className = 'holiday';
+      tit.innerHTML = txt;
+      daysWeek.appendChild(tit);
+    } else if (txt === 4 | txt === 11 | txt === 18 | txt === 25) {
+      tit.className = 'day friday';
+      tit.innerHTML = txt;
+      daysWeek.appendChild(tit);
+    } else {
+      tit.innerHTML = txt;
+      tit.className = 'day';
+      daysWeek.appendChild(tit);
+    }
+  };
+}
+function createHoliday(feriado) {
+  const Holiday = document.querySelector('.buttons-container');
+  const createTitle = document.createElement('h3');
+  const createInput = document.createElement('input');
+  const createButton = document.createElement('button');
+  const creatSection = document.createElement('section');
+  creatSection.className = 'tasks-container';
+  createTitle.innerHTML = 'FERIADOS';
+  // createTitle.className = 'tasks-container';
+  createButton.innerHTML = 'Adicionar';
+  createButton.className = '#btn-holiday';
+  createInput.innerHTML = feriado;
+  createInput.className = '#btn-holiday';
+  createInput.placeholder = 'Escreva o feriado';
+  Holiday.appendChild(creatSection);
+  const holiday = document.querySelector('section');
+  holiday.appendChild(createTitle);
+  holiday.appendChild(createInput);
+  holiday.appendChild(createButton);
+}
 
-// Escreva seu código abaixo.
+createDaysOfTheWeek()
+createDaysOfMonth()
+createHoliday()
