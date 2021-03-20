c566c4632d326d72719bc554d3f4f819ecd138d8
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-03-20 12:15:16 -0300

    funcao verifica data

--- exercises/bloco_6/html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ exercises/bloco_6/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -15,3 +15,31 @@ function criarResumo () {
   })
 }
 criarResumo();
+
+function criarOpcaoEstados() {
+  const pegarPrincipal = document.getElementById('estado');
+  const estados = ['Selecione seu estado', 'AC', 'AL', 'AM', 'AP', 'BA', 'CE', 'DF', 'ES', 'GO', 'MA', 'MG', 'MS', 'MT', 'PA', 'PB', 'PE', 'PI', 'PR', 'RJ', 'RN', 'RO', 'RR', 'RS', 'SC', 'SE', 'SP', 'TO'];
+
+  for (let i = 0; i < estados.length; i += 1) {
+    const criarOpcao = document.createElement('option');
+    criarOpcao.innerText = estados[i];
+    criarOpcao.value = estados[i];
+    pegarPrincipal.appendChild(criarOpcao);
+  }
+}
+criarOpcaoEstados();
+
+function verificaData(data) {
+  if (data.indexOf('/') === 2 || data.indexOf('/')) {
+    const dia = data.substr(0,2);
+    const mes = data.substr(3,2);
+    const ano = data.substr(6,4);
+    if (dia >= 0 && dia <=31 && mes >= 1 && mes <= 12 && ano < 0 && ano.length === 4) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+function 
