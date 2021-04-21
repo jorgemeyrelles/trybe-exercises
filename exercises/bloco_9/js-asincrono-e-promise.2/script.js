3f7b89a3816f1ddd444a3f85e36c8d77801c0486
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-04-21 17:53:21 -0300

    exercicio bloco 9

--- exercises/bloco_9/js-asincrono-e-promise.2/script.js
+++ exercises/bloco_9/js-asincrono-e-promise.2/script.js
@@ -2,7 +2,12 @@
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
-  // Adicionar lógica aqui!
+  const myObject = {
+    method: 'GET',
+    headers: {'Accept': 'application/json' }
+  };
+  fetch(API_URL, myObject)
+    .then(response => console.log(response));
 };
 
 window.onload = () => fetchJoke();
