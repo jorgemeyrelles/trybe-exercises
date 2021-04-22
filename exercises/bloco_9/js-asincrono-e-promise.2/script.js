96e9640a1af1aff515961ff337e11bf9e3d18aca
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-04-22 11:01:10 -0300

    exercicio bloco 9 - finalizada

--- exercises/bloco_9/js-asincrono-e-promise.2/script.js
+++ exercises/bloco_9/js-asincrono-e-promise.2/script.js
@@ -45,12 +45,31 @@ const onScreem3 = () => {
       arr: array,
       sum: soma2
     };
-    (obj.sum < 8000) ? resolve(obj.sum): reject('Promise rejeitada');
+    (obj.sum < 8000) ? resolve(obj.sum): reject('Promise rejeitada!');
   });
   number
-    .then((numb) => document.getElementById('jokeContainer').innerText = `Promise aceita: ${numb}`)
+    .then((numb) => document.getElementById('jokeContainer').innerText = `Promise aceita: ${[2, 3, 5, 10].map((n) => numb / n)}`)
     .catch((err) => document.getElementById('jokeContainer').innerText = err);
 }
 
+// Exercicio 4 e 5
+const onScreem4 = () => {
+  const number = new Promise((resolve, reject) => {
+    const array = [];
+    for (let i = 0; i < 10; i += 1) {
+      array[i] = Math.pow(Math.floor(Math.random() * 51), 2);
+    }
+    const soma = array.map((values) => values).reduce((acc, e) => acc + e);
+    const soma2 = array.reduce((acc, e) => acc + e);
+    const obj = {
+      arr: array,
+      sum: soma2
+    };
+    (obj.sum < 8000) ? resolve(obj): reject('Esta Promise é maior que 8000. Está quebrada!');
+  });
+  number
+    .then((numb) => document.getElementById('jokeContainer').innerText = `Promise aceita: ${numb.arr} e ${numb.sum}`)
+    .catch((err) => document.getElementById('jokeContainer').innerText = err);
+}
 
-window.onload = () => onScreem3();
+window.onload = () => onScreem4();
