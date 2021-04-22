cc9b1ccc5474aa2d9b1a66265545b0ab943e431b
Jorge Meyrelles Jr <jotaengpuc@gmail.com>
2021-04-22 10:54:41 -0300

    exercicio bloco 9

--- exercises/bloco_9/js-asincrono-e-promise.2/script.js
+++ exercises/bloco_9/js-asincrono-e-promise.2/script.js
@@ -1,4 +1,4 @@
-// apiScript.js
+// Exercicio 1
 const API_URL = 'https://icanhazdadjoke.com/';
 
 const fetchJoke = () => {
@@ -7,7 +7,50 @@ const fetchJoke = () => {
     headers: {'Accept': 'application/json' }
   };
   fetch(API_URL, myObject)
-    .then(response => console.log(response));
+    .then(response => response.json())
+    .then(msg => document.getElementById('jokeContainer').innerText = msg.joke);
 };
 
-window.onload = () => fetchJoke();
+// Exercicio 2
+const onScreem = () => {
+  const number = new Promise((resolve, reject) => {
+    const array = [];
+    for (let i = 0; i < 10; i += 1) {
+      array[i] = Math.pow(Math.floor(Math.random()*51), 2);
+    }
+    const soma = array.map((values) => values).reduce((acc, e) => acc + e);
+    const soma2 = array.reduce((acc, e) => acc + e);
+    const obj = {
+      arr: array,
+      sum: soma2
+    };
+    (obj.sum < 8000) ? resolve() : reject();
+  });
+  number
+    .then(() => document.getElementById('jokeContainer').innerText = 'Promise aceita!')
+    .catch(() => document.getElementById('jokeContainer').innerText = 'Promise rejeitada');
+}
+
+// Exercicio 3
+const onScreem3 = () => {
+  const number = new Promise((resolve, reject) => {
+    const array = [];
+    for (let i = 0; i < 10; i += 1) {
+      array[i] = Math.pow(Math.floor(Math.random() * 51), 2);
+    }
+    const soma = array.map((values) => values).reduce((acc, e) => acc + e);
+    const soma2 = array.reduce((acc, e) => acc + e);
+    const obj = {
+      method: 'GET',
+      arr: array,
+      sum: soma2
+    };
+    (obj.sum < 8000) ? resolve(obj.sum): reject('Promise rejeitada');
+  });
+  number
+    .then((numb) => document.getElementById('jokeContainer').innerText = `Promise aceita: ${numb}`)
+    .catch((err) => document.getElementById('jokeContainer').innerText = err);
+}
+
+
+window.onload = () => onScreem3();
